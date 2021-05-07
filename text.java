22ae26abe8109e8377ba9a62e245b358a19688a8
squirosv <83682837+squirosv@users.noreply.github.com>
2021-05-06 19:51:25 -0600

    Update text.java

--- text.java
+++ text.java
@@ -2,6 +2,6 @@ import java.io.*;
 
 class text{
     public static void main(String[] args) throws IOException{
-        System.out.println("Hey what's up")
+        System.out.println("Hey what's up");
     }
 }
